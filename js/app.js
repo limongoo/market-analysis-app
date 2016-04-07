01a7a68e514f8c0c0aecaed65df9c05a2bd57839
Ivan Limongan <ivan.limongan@gmail.com>
2016-04-07 16:59:33 -0700

    started working on app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
-var imgpath = 'img/';
+console.log("start");
+
 var img = ['bag.jpg', 'banana.jpg', 'boots.jpg', 'chair.jpg', 'cthulhu.jpg', 'dragon.jpg', 'pen.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'unicorn.jpg', 'usb.jpg', 'water_can.jpg', 'wine_glass.jpg'];
 
 
-// Random function
-var getRandom = function() {
-    return Math.floor(Math.random() * img.length);
+// Random image function
+function getRandom() {
+    var random =  Math.floor(Math.random() * img.length);
+
+    var imgLeft = document.getElementById('img1').src = "img/" + img[random];
+    var imgMid = document.getElementById('img2').src = "img/" + img[random];
+    var imgRight = document.getElementById('img3').src = "img/" + img[random];
+
+
 }
+getRandom();
+
+// Display img function
+
+
+
+
+// Tally function
