acee015ef5aa62795cac43ae15efa7c2917c8091
Ivan Limongan <ivan.limongan@gmail.com>
2016-04-07 10:16:37 -0700

    basic setup, start on js and index

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var imgpath = 'img/';
+var img = ['bag.jpg', 'banana.jpg', 'boots.jpg', 'chair.jpg', 'cthulhu.jpg', 'dragon.jpg', 'pen.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'unicorn.jpg', 'usb.jpg', 'water_can.jpg', 'wine_glass.jpg'];
+
+
+// Random function
+var getRandom = function() {
+    return Math.floor(Math.random() * img.length);
+}
