1c9fb8b392d28c7971dcfd778c23fd3b1c11a7bf
Ivan Limongan <ivan.limongan@gmail.com>
2016-04-07 18:25:19 -0700

    added normalize and skeleton css, and have random image to display working

--- js/app.js
+++ js/app.js
@@ -5,18 +5,31 @@ var img = ['bag.jpg', 'banana.jpg', 'boots.jpg', 'chair.jpg', 'cthulhu.jpg', 'dr
 
 // Random image function
 function getRandom() {
-    var random =  Math.floor(Math.random() * img.length);
+    return Math.floor(Math.random() * img.length);
 
-    var imgLeft = document.getElementById('img1').src = "img/" + img[random];
-    var imgMid = document.getElementById('img2').src = "img/" + img[random];
-    var imgRight = document.getElementById('img3').src = "img/" + img[random];
-
+    // var imgLeft = document.getElementById('img1').src = "img/" + img[random];
+    // var imgMid = document.getElementById('img2').src = "img/" + img[random];
+    // var imgRight = document.getElementById('img3').src = "img/" + img[random];
 
 }
-getRandom();
+
 
 // Display img function
+function display () {
+  var imgOne = document.getElementById('img1')
+  var imgTwo = document.getElementById('img2')
+  var imgThree = document.getElementById('img3')
 
+  img1 = getRandom();
+  imgOne.src = "img/" + img[img1];
+
+  img2 = getRandom();
+  imgTwo.src = "img/" + img[img2];
+
+  img3 = getRandom();
+  imgThree.src = "img/" + img[img3];
+}
+display();
 
 
 
