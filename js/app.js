5db1d0c7521d76200e5ca43d7c31b4455138e1df
Ivan Limongan <ivan.limongan@gmail.com>
2016-04-09 09:01:18 -0700

    adjusted the click function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 console.log("start");
 
-// Image array
+// Global variables
 var imgOne = document.getElementById('img1');
 var imgTwo = document.getElementById('img2');
 var imgThree = document.getElementById('img3');
-
 var productImage = [];
 
+
+// Object Constructor
 var product = function(productName, imgPath) {
   this.productName = productName;
   this.imgPath = imgPath;
@@ -14,6 +15,8 @@ var product = function(productName, imgPath) {
   productImage.push(this);
 }
 
+
+// Images
 var bag = new product ('bag','img/bag.jpg');
 var banana = new product ('banana','img/banana.jpg');
 var boots = new product ('boots','img/boots.jpg');
@@ -30,38 +33,27 @@ var watercan = new product ('watercan','img/water_can.jpg');
 var wineglass = new product ('wineglass','img/wine_glass.jpg');
 
 
-
-
-
 // Random image function
 function getRandom() {
     return Math.floor(Math.random() * productImage.length);
-    // var imgLeft = document.getElementById('img1').src = "img/" + img[random];
-    // var imgMid = document.getElementById('img2').src = "img/" + img[random];
-    // var imgRight = document.getElementById('img3').src = "img/" + img[random];
 }
 
 
 // Display img function
 function display () {
-
-
   img1 = getRandom();
-  // var i1 = imgOne.src = "img/" + img[img1];
   imgOne.src = productImage[img1].imgPath;
 
   img2 = getRandom();
     while (img2 === img1) {
       img2 = getRandom();
     };
-  // var i2 = imgTwo.src = "img/" + img[img2];
   imgTwo.src = productImage[img2].imgPath;
 
   img3 = getRandom();
     while (img3 === img2 || img3 === img1) {
       img3 = getRandom();
     }
-  // var i3 = imgThree.src = "img/" + img[img3];
   imgThree.src = productImage[img3].imgPath;
 
   console.log(productImage[img1]);
@@ -71,12 +63,12 @@ function display () {
 display();
 
 
+// Click event listener
+imgOne.addEventListener('click', counterClick);
+imgTwo.addEventListener('click', counterClick);
+imgThree.addEventListener('click', counterClick);
 
 
-
-imgOne.addEventListener('click', click1);
-imgTwo.addEventListener('click', click2);
-imgThree.addEventListener('click', click3);
 
 // Counter function
 var totalClick = 0;
@@ -84,38 +76,57 @@ var img1 = 0;
 var img2 = 0;
 var img3 = 0;
 
-function click1() {
+
+// counterClick function
+function counterClick() {
   productImage[img1].counter++;
-  totalClick++;
-  console.log(productImage[img1].productName);
-  console.log(totalClick);
-  display();
-}
-function click2() {
   productImage[img2].counter++;
-  totalClick++;
-  console.log(productImage[img2].productName);
-  console.log(totalClick);
-  display();
-}
-function click3() {
   productImage[img3].counter++;
   totalClick++;
-  console.log(productImage[img3].productName);
+
+  if (totalClick === 15) {
+    resultButton.removeAttribute('hidden');
+  }
+
   console.log(totalClick);
   display();
 }
 
+funtion displayCounter() {
+  totalClick
+}
 
-// function counterClick(product) {
-//   product.counter++;
+var resultButton = document.getElementById('showResults');
+
+// imgOne.addEventListener('click', click1);
+// imgTwo.addEventListener('click', click2);
+// imgThree.addEventListener('click', click3);
+
+// function click1() {
+//   productImage[img1].counter++;
 //   totalClick++;
-//
-//   // console.log("counter function");
-//   // console.log(totalClick);
-//
+//   console.log(productImage[img1].productName);
+//   console.log(totalClick);
 //   display();
 // }
+// function click2() {
+//   productImage[img2].counter++;
+//   totalClick++;
+//   console.log(productImage[img2].productName);
+//   console.log(totalClick);
+//   display();
+// }
+// function click3() {
+//   productImage[img3].counter++;
+//   totalClick++;
+//   console.log(productImage[img3].productName);
+//   console.log(totalClick);
+//   display();
+// }
+
+
+
+
 
 
 // Display Result
